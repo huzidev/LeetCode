e48c67c11e43b5949e8c693919aee4375a660df5
huzidev <huzaifa.iqdev@gmail.com>
2023-08-28 17:32:18 +0500

    anagram problem:

---
+++ Practice/index.js
@@ -0,0 +1,26 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// Example 1:
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+
+// Example 2:
+// Input: s = "rat", t = "car"
+// Output: false
+
+let s = "anagram";
+let t = "nagaram";
+
+let val1 = s.toLowerCase().sort()
+let val2 = t.toLowerCase().sort()
+
+console.log(val1);
+console.log(val2);
+
+// after .split('') it'll became [ 'a', 'n', 'a', 'g', 'r', 'a', 'm' ]
+// after sort both s and t became same values [ 'a', 'a', 'a', 'g', 'm', 'n', 'r' ]
+// after .join('') the values became string from array aagmnr hence it'll became easy to compare both value after sorting and spliting
+
+return val1.length === val2.length && val1 === val2 ? true : false;
