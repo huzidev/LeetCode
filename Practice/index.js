29cb1bf7a692b4ea3f752c0bf4c3230b7f227536
huzidev <huzaifa.iqdev@gmail.com>
2023-11-30 23:52:34 +0500

    fixing new length loops

--- Practice/index.js
+++ Practice/index.js
@@ -1,45 +1,17 @@
-// let nums = [1, 2, 3, 4];
-let nums = [2, 3];
-// if (nums.length === 2) {
-//     console.log('nums', nums.reverse());
-// }
-// let result = [];
-// let newArray = [];
-// let val;
+let nums = [1, 2, 3, 4];
+let result = [];
+let newArray = [];
+let val;
 
-// for (let i = 0; i < nums.length; i++) {
-//     newArray = nums.filter((v, index) => index !== i);
-//     console.log("New array", newArray);
-//     for (let j = 0; j < newArray.length - 1; j++) {
-//         if (j !== 0) {
-//             val = val * newArray[j + 1];
-//         } else {
-//             val = newArray[j] * newArray[j + 1];
-//         } 
-//     }
-//     result.push(val);
-// }
-// console.log("Result", result.push(nums[1, 0]));
-
-if (nums.length === 2) {
-    console.log("nums", nums.reverse());
-    return nums.reverse();
-} else {
-    let result = [];
-    let newArray = [];
-    let val;
-
-    for (let i = 0; i < nums.length; i++) {
-        newArray = nums.filter((v, index) => index !== i);
-        for (let j = 0; j < newArray.length - 1; j++) {
-            if (j !== 0) {
-                val = val * newArray[j + 1];
-            } else {
-                val = newArray[j] * newArray[j + 1];
-            }
-        }
-        result.push(val);
+for (let i = 0; i < nums.length; i++) {
+    newArray = nums.filter((v, index) => index !== i);
+    for (let j = 0; j < newArray.length - 1; j++) {
+        if (j !== 0) {
+            val = val * newArray[j + 1];
+        } else {
+            val = newArray[j] * newArray[j + 1];
+        } 
     }
-    // return(result);
-    // console.log("nums", nums.reverse());
+    result.push(val);
 }
+console.log("Result ", result);
