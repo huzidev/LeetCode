7980b9dfae18682f796db16f953fbc7cee751a9f
huzidev <huzaifa.iqdev@gmail.com>
2023-11-30 15:15:45 +0500

    fixing time limit error

--- Practice/index.js
+++ Practice/index.js
@@ -1,24 +1,45 @@
 // let nums = [1, 2, 3, 4];
-let nums = [0, 0];
+let nums = [2, 3];
+// if (nums.length === 2) {
+//     console.log('nums', nums.reverse());
+// }
+// let result = [];
+// let newArray = [];
+// let val;
 
-let result = [];
-let newArray = [];
-let val;
+// for (let i = 0; i < nums.length; i++) {
+//     newArray = nums.filter((v, index) => index !== i);
+//     console.log("New array", newArray);
+//     for (let j = 0; j < newArray.length - 1; j++) {
+//         if (j !== 0) {
+//             val = val * newArray[j + 1];
+//         } else {
+//             val = newArray[j] * newArray[j + 1];
+//         } 
+//     }
+//     result.push(val);
+// }
+// console.log("Result", result.push(nums[1, 0]));
 
 if (nums.length === 2) {
-    nums.forEach((value, i) => value * nums[index + 1])
-}
+    console.log("nums", nums.reverse());
+    return nums.reverse();
+} else {
+    let result = [];
+    let newArray = [];
+    let val;
 
-for (let i = 0; i < nums.length; i++) {
-    newArray = nums.filter((v, index) => index !== i);
-    console.log("New array", newArray);
-    for (let j = 0; j < newArray.length - 1; j++) {
-        if (j !== 0) {
-            val = val * newArray[j + 1];
-        } else {
-            val = newArray[j] * newArray[j + 1];
-        } 
+    for (let i = 0; i < nums.length; i++) {
+        newArray = nums.filter((v, index) => index !== i);
+        for (let j = 0; j < newArray.length - 1; j++) {
+            if (j !== 0) {
+                val = val * newArray[j + 1];
+            } else {
+                val = newArray[j] * newArray[j + 1];
+            }
+        }
+        result.push(val);
     }
-    result.push(val);
+    // return(result);
+    // console.log("nums", nums.reverse());
 }
-console.log("Result", result);
