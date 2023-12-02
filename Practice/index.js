2c22e263cf927f540d063b43e6e7de6c046bf55c
huzidev <huzaifa.iqdev@gmail.com>
2023-12-02 12:32:54 +0500

    js reduce product

--- Practice/index.js
+++ Practice/index.js
@@ -1,15 +1,17 @@
 let nums = [1, 2, 3, 4];
+// let nums = [-1, 1, 0, -3, 3];
 let result = [];
 let newArray = [];
 
-console.log("");
+// console.log("product", nums.reduce((a, b) => a * b), 1);
 
-// for (let i = 0; i < nums.length; i++) {
-//     newArray = nums.filter((v, index) => index !== i);
-//     let val = 1;
-//     for (let j = 0; j < newArray.length; j++) {
-//         val = val * newArray[j];
-//     }
-//     result.push(val);
-// }
-// console.log("Result ", result);
+for (let i = 0; i < nums.length; i++) {
+    newArray = nums.filter((v, index) => index !== i);
+    let val = 1;
+    val = newArray.reduce((a, b) => a * b);
+    // for (let j = 0; j < newArray.length; j++) {
+    //     val = val * newArray[j];
+    // }
+    result.push(val);
+}
+console.log("Result ", result);
