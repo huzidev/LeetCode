73b565e5261005d7fabb0e8d52b1c8608b7ca556
huzidev <huzaifa.iqdev@gmail.com>
2023-12-02 17:04:25 +0500

    product of array completed

--- Arrays & Hashing/6 Product of Array Except Self/index.js
+++ Arrays & Hashing/6 Product of Array Except Self/index.js
@@ -49,3 +49,20 @@ for (let i = nums.length - 1; i >= 0; i--) {
 }
 
 return result;
+
+// different code will give Time Limit Exceeds error
+// let result = [];
+// let newArray = [];
+
+// let result = nums.map((v, i) => nums.filter((val, index) => i !== index).reduce((a, b) => a * b), 1);
+
+// for (let i = 0; i < nums.length; i++) {
+//     console.log("Res", nums.map((start, index) => index))
+//     // newArray = nums.filter((v, index) => index !== i);
+//     // let val = 1;
+//     // val = newArray.reduce((a, b) => a * b, 1);
+//     // // for (let j = 0; j < newArray.length; j++) {
+//     // //     val = val * newArray[j];
+//     // // }
+//     // result.push(val);
+// }
