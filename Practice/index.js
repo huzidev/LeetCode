24102abb4ebbacc8857bfc1080d4b3b8a75d8a20
huzidev <huzaifa.iqdev@gmail.com>
2023-12-02 21:08:15 +0500

    fixing rows and columns value

--- Practice/index.js
+++ Practice/index.js
@@ -10,10 +10,13 @@ let board = [
   [".", ".", ".", ".", "8", ".", ".", "7", "9"],
 ];
 
-let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+let arr = [
+    [1, 2, 3], 
+    [4, 5, 6], 
+    [7, 8, 9]]
+;
 
-for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board.length; j++) {
-        board[i] === board[j] ? console.log("Flase") : console.log("true");;
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
     }
 }
