851d93379aa3487d7638818b106b656b5bbf3f8c
huzidev <huzaifa.iqdev@gmail.com>
2023-12-02 12:55:07 +0500

    result array worked

--- Practice/index.js
+++ Practice/index.js
@@ -1,17 +1,21 @@
-let nums = [1, 2, 3, 4];
-// let nums = [-1, 1, 0, -3, 3];
-let result = [];
+// let nums = [1, 2, 3, 4];
+let nums = [-1, 1, 0, -3, 3];
+// let result = [];
 let newArray = [];
 
-// console.log("product", nums.reduce((a, b) => a * b), 1);
 
-for (let i = 0; i < nums.length; i++) {
-    newArray = nums.filter((v, index) => index !== i);
-    let val = 1;
-    val = newArray.reduce((a, b) => a * b);
-    // for (let j = 0; j < newArray.length; j++) {
-    //     val = val * newArray[j];
-    // }
-    result.push(val);
-}
+// console.log("Test", nums.map((val, index) => nums.filter((value, i) => index !== i).reduce((a, b) => a * b), 1));
+
+let result = nums.map((v, i) => nums.filter((val, index) => i !== index).reduce((a, b) => a * b), 1); 
+
+// for (let i = 0; i < nums.length; i++) {
+//     console.log("Res", nums.map((value, index) => index))
+//     // newArray = nums.filter((v, index) => index !== i);
+//     // let val = 1;
+//     // val = newArray.reduce((a, b) => a * b, 1);
+//     // // for (let j = 0; j < newArray.length; j++) {
+//     // //     val = val * newArray[j];
+//     // // }
+//     // result.push(val);
+// }
 console.log("Result ", result);
