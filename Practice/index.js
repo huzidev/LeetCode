22b2e0f91b327152f2d1fa8cd7fa3047be384f53
huzidev <huzaifa.iqdev@gmail.com>
2023-12-02 13:02:53 +0500

    updating on self array error

--- Practice/index.js
+++ Practice/index.js
@@ -4,9 +4,9 @@ let nums = [-1, 1, 0, -3, 3];
 let newArray = [];
 
 
-// console.log("Test", nums.map((val, index) => nums.filter((value, i) => index !== i).reduce((a, b) => a * b), 1));
+console.log("Test", nums.map((val, index) => val))
 
-let result = nums.map((v, i) => nums.filter((val, index) => i !== index).reduce((a, b) => a * b), 1); 
+// let result = nums.map((v, i) => nums.filter((val, index) => i !== index).reduce((a, b) => a * b), 1); 
 
 // for (let i = 0; i < nums.length; i++) {
 //     console.log("Res", nums.map((value, index) => index))
