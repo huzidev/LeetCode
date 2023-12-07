5573e0d350965c115b2b2563e04fc7092ab8a16b
huzidev <huzaifa.iqdev@gmail.com>
2023-12-07 12:47:35 +0500

    new array set

--- Practice/index.js
+++ Practice/index.js
@@ -81,9 +81,10 @@
 
 
 
-// let nums = [9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6];
-// nums.sort((a, b) => a - b);
-// let newArr = new Set(nums);
+let nums = [9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6];
+nums.sort((a, b) => a - b);
+let newArr = [...new Set(nums)];
+console.log("New arr", newArr);
 // let newRes = [...newArr]
 // let result = [];
 // console.log('New array', newRes);
