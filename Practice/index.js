7588f6277c472dde0ab26a61aa101241ca2eb300
huzidev <huzaifa.iqdev@gmail.com>
2023-12-07 18:58:46 +0500

    sub-boxes for sudoky

--- Practice/index.js
+++ Practice/index.js
@@ -23,10 +23,8 @@ let box = [];
 // j for column
 for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-        let value = board[i][j];
-        validBox(board, i, j, value)
         // for Rows
-        value === '.' ? '' : row.push(board[i][j]) 
+        board[i][j] === '.' ? '' : row.push(board[i][j]) 
         // for Columns
         board[j][i] === '.' ? '' : column.push(board[j][i])
 
@@ -41,98 +39,3 @@ for (let i = 0; i < 9; i++) {
         }
     }
 }
-
-function validBox(board, row, column, value) {
-    const startRow = row - (row % 3);
-    const startCol = column - (column % 3);
-    for (let i = startRow; i < startRow; i++) {
-        for (let j = startCol; j < startRow; j++) {
-            
-        }
-    }
-}
-
-
-
-// for (let i = 0; i < board.length; i++) {
-//     board.map(col => col[i] === '.' ? '' : column.push(col[i]));
-//     board[i] === '.' ? '' : row.push(board[i]);
-//     // console.log("Result col", column.length !== new Set(column).size ? false : true);
-//     // console.log("Result row", row.length !== new Set(row).size ? false : true);
-//     column = [];
-//     row = [];
-
-//     // let hold = i === 0 ? 0 : + 3;
-//     // for (let j = hold; j < 6; j++) {
-//         //     for (let k = hold; k < 6; k++) {
-//             //         // board[j][k] === '.' ? '' : box.push(board[j][k]);
-//             //         // console.log("3x3 Result", box.length !== new Set(box) ? false : true);
-//             //         box.push(board[j][k])
-//             //     }
-//             // }
-//             // console.log('box', box);
-//         }
-
-// let hold = 0;
-// for (let j = hold; j < 3; j++) {
-//     for (let k = hold; k < 3; k++) {
-//         // board[j][k] === '.' ? '' : box.push(board[j][k]);
-//         // console.log("3x3 Result", box.length !== new Set(box) ? false : true);
-//         box.push(board[j][k])
-//     }
-// }
-// console.log('box', box);
-
-
-
-
-
-
-
-
-
-
-
-// let nums = [9, 1, -3, 2, 4, 8, 3, -1, 6, -2, -4, 7];
-// // let nums = [100, 4, 200, 1, 3, 2];
-// if (!nums.length) return 0;
-// nums.sort((a, b) => a - b);
-// let set = new Set(nums);
-// console.log("set", set);
-
-// let maxSequence = 0;
-// let currentSequence = 1;
-
-// for (const i of set) {
-//     let hold = i;
-
-//     if (set.has(hold + 1)) {
-//         currentSequence++;
-//     } else {
-//         if (maxSequence < currentSequence) {
-//             maxSequence = currentSequence;
-//             console.log("max seq", maxSequence);
-//         }
-//         currentSequence = 1;
-//     }
-// }
-// console.log("result", Math.max(currentSequence, maxSequence));
-
-
-// if (!nums.length) return 0;
-// nums.sort((a, b) => a - b);
-// let set = new Set(nums);
-// let maxSequence = 0;
-// let currentSequence = 1;
-// set.forEach((val, i) => {
-//     let hold = i !== set.size ? i + 1 : '';
-//     if (hold === val + 1) {
-//         currentSequence++;
-//     } else {
-//         if (maxSequence < currentSequence) {
-//             maxSequence = currentSequence;
-//         }
-//         currentSequence = 1;
-//     }
-// })
-// return Math.max(currentSequence, maxSequence)
